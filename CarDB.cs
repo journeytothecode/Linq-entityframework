d8a0f0f74aafceef878ff75faeef0591486104b6
Tom Dinh <tomdinhnz@gmail.com>
2016-11-07 19:24:20 +1300

    setup entity framework and local database

---
+++ CarDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace LinqInEntityFramework
+{
+    public class CarDB : DbContext
+    {
+        // class represent database for Entity framework to access
+        public DbSet<Car> Cars { get; set; } // a table name Cars this use for Linq
+
+    }
+}
