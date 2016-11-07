0864d68ec6f9fedf12563b623bb563a338bd7746
Tom Dinh <tomdinhnz@gmail.com>
2016-11-07 20:33:54 +1300

    Insert Data in to CarDB

---
+++ CarDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace LinqInEntityFramework
+{
+    public class CarDB : DbContext
+    {
+        // class represent database for Entity framework to access
+
+        public DbSet<Car> Cars { get; set; } // a table name Cars this use for Linq
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace LinqInEntityFramework
 {
@@ -10,6 +12,66 @@ namespace LinqInEntityFramework
     {
         static void Main(string[] args)
         {
+
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CarDB>());
+            InsertData();
+            QueryData();
+
+
+            //var cars = ConvertCSV("fuel.csv");
+            //foreach (var i in cars)
+            //{
+            //    Console.WriteLine($"{i.Name} : {i.Manufacturer}");
+            //}
+        }
+
+        private static void QueryData()
+        {
+
+        }
+
+        private static void InsertData()
+        {
+            var cars = ConvertCSV("fuel.csv"); // call to read data in memory
+            var db = new CarDB();
+
+            if (!db.Cars.Any())
+            {
+                foreach (var car in cars)
+                {
+                    db.Cars.Add(car);
+                }
+                db.SaveChanges();
+            }
+
+
+        }
+
+        private static List<Car> ConvertCSV(string path)
+        {
+
+            return
+                File.ReadAllLines(path)
+                .Skip(1)
+                .Where(c => c.Length > 1)
+                .Select(c =>
+                {
+                    var columns = path.Split(',');
+                    return new Car
+                    {
+                        Year = int.Parse(columns[0]),
+                        Manufacturer = columns[1],
+                        Name = columns[2],
+                        Displacement = Double.Parse(columns[3]),
+                        Cylinders = int.Parse(columns[4]),
+                        City = int.Parse(columns[5]),
+                        Highway = int.Parse(columns[6]),
+                        Combined = int.Parse(columns[7])
+                    };
+                })
+                .ToList();
+
+
         }
     }
 }
